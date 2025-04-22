493cbb4968f2d8d0c2ee646303d1ff7517d0a338
tutatrumpet <letu24gx@student.ju.se>
2025-04-22 22:43:05 +0200

    updated goals and about

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ const panel = document.querySelector(".dropdown-panel");
 button.addEventListener("click", () => {
   panel.classList.toggle("active");
 });
+
+//dropdown "create goals" - Goals page
+const button2 = document.querySelector(".dropdown-button2");
+const panel2 = document.querySelector(".dropdown-panel2");
+
+button2.addEventListener("click", () => {
+  panel2.classList.toggle("active");
+});
