d7cb031d540f45181e253d5b2c995308e0c60d44
tutatrumpet <letu24gx@student.ju.se>
2025-04-28 10:32:09 +0200

    search box

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
 });
+
+//search bar
+function searchPage() {
+  let query = document.getElementById("search-box").value.toLowerCase();
+  if (query === "about") {
+    window.location.href = "about.html";
+  } else if (query === "community") {
+    window.location.href = "Community.html";
+  } else if (query === "courses") {
+    window.location.href = "courses.html";
+  } else if (query === "goals") {
+    window.location.href = "Goals.html";
+  } else {
+    window.location.href = "search.html";
+  }
+}
