7259916dda7d977d0c528a1385aa3f0c5fc9a5bd
aonajin <onag24di@student.ju.se>
2025-05-01 23:59:01 +0200

    Back to top arrow implemented

--- js/main.js
+++ js/main.js
@@ -5,3 +5,18 @@ hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
 });
+
+const backToTopButton = document.getElementById("backToTop");
+
+// activate scroll after 300px
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopButton.classList.add("visible");
+  } else {
+    backToTopButton.classList.remove("visible");
+  }
+});
+
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
