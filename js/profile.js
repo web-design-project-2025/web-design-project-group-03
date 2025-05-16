cc45b87c97a8b801045fd0f5f4d31ec47bf32c24
aonajin <onag24di@student.ju.se>
2025-05-16 14:45:17 +0200

    styled goals, simplified js

--- js/profile.js
+++ js/profile.js
@@ -1,15 +1,9 @@
-//dropdown "current goals" - Goals page
-const button = document.querySelector(".dropdown-button");
-const panel = document.querySelector(".dropdown-panel");
-
-button.addEventListener("click", () => {
-  panel.classList.toggle("active");
-});
-
-//dropdown "create goals" - Goals page
-const button2 = document.querySelector(".dropdown-button2");
-const panel2 = document.querySelector(".dropdown-panel2");
-
-button2.addEventListener("click", () => {
-  panel2.classList.toggle("active");
+// click event to all dropdown buttons with data-dropdown-btn attribute
+document.querySelectorAll("[data-dropdown-btn]").forEach((button) => {
+  button.addEventListener("click", () => {
+    const panel = button.nextElementSibling;
+    if (panel) {
+      panel.classList.toggle("active");
+    }
+  });
 });
